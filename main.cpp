b374f1c34807a24411fd01f2e7db063e10592007
Maxim Eryomenko <moeryomenko@gmail.com>
2018-12-14 04:14:57 +0300

    add bench function

--- main.cpp
+++ main.cpp
@@ -4,22 +4,24 @@
 #include "generator.hpp"
 #include "bubble.hpp"
 
+template<typename T, class sort_func>
+void bench(std::vector<T> vector, sort_func sort) {
+	auto begin {std::chrono::high_resolution_clock::now()};
+	bubble_sort(bubble);
+	auto end {std::chrono::high_resolution_clock::now()};
+
+	std::cout << "Bubble sort time: "
+		<< std::chrono::duration<double, std::milli>{end - begin}.count()
+		<< "ms" << std::endl;
+}
+
 int main() {
 	// Generate random test array
 	std::vector<int> test(50'000);
 	generate(test, 1, 100);
 
 	// Bench bubble sort
-	{
-		auto bubble = test;
-		auto begin {std::chrono::high_resolution_clock::now()};
-		bubble_sort(bubble);
-		auto end {std::chrono::high_resolution_clock::now()};
-
-		std::cout << "Bubble sort time: "
-			<< std::chrono::duration<double, std::milli>{end - begin}.count()
-			<< "ms" << std::endl;
-	}
+	bench(test, bubble_sort<int>);
 
 	return 0;
 }
