7d2a38a010f11e7229ac0d441b0fda45f270614d
Maxim Eryomenko <moeryomenko@gmail.com>
2018-12-14 04:09:33 +0300

    Add bench bubble sort

--- main.cpp
+++ main.cpp
@@ -2,11 +2,24 @@
 #include <chrono>
 
 #include "generator.hpp"
+#include "bubble.hpp"
 
 int main() {
 	// Generate random test array
 	std::vector<int> test(50'000);
 	generate(test, 1, 100);
+
+	// Bench bubble sort
+	{
+		auto bubble = test;
+		auto begin {std::chrono::high_resolution_clock::now()};
+		bubble_sort(bubble);
+		auto end {std::chrono::high_resolution_clock::now()};
+
+		std::cout << "Bubble sort time: "
+			<< std::chrono::duration<double, std::milli>{end - begin}.count()
+			<< "ms" << std::endl;
+	}
 
 	return 0;
 }
